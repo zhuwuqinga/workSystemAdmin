f1773a24fa89567ad6209cd53ec29e0e79eda378
zhuwuqing <1036716591@qq.com>
2021-08-15 19:03:28 +0800

    ini

---
+++ src/router/seleDefine.js
@@ -0,0 +1,16 @@
+// import Main from '@/components/main'
+// import parentView from '@/components/parent-view'
+
+const seleDefine = [
+  // {
+  //   path: '',
+  //   name: 'doc',
+  //   meta: {
+  //     title: '文档',
+  //     href: 'https://lison16.github.io/iview-admin-doc/#/',
+  //     icon: 'ios-book'
+  //   }
+  // }
+]
+
+export default seleDefine
